1e9e2d875678e48eb7c25589892f2d04275d2bd3
Kevin Sajan <kevsaj@gmail.com>
2021-04-10 00:34:22 -0400

    deployment test

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/pages/*.{js,jsx,ts,tsx}', './src/components/*.{js,jsx,ts,tsx}', './public/**/*.html'],
+  purge: ['./src/*.{js,jsx,ts,tsx}', './src/components/*.{js,jsx,ts,tsx}', './public/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
