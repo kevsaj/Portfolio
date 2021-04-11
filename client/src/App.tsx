3d4f2ccb1f48468983a2b46fad81a4e37ca36f97
Kevin Sajan <kevsaj@gmail.com>
2021-04-11 15:34:24 -0400

    header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from './components/Header/index'
 
 function App() {
   return (
-    <div className="logo flex flex-col w-full h-full">
-      <header className="logo pt-20 mx-auto">
-        <div className="logo w-full h-8 text-xl font-bold text-white hover:text-black hover:bg-pink-200">
-          <a
-            className="p-10 py-20 items-center logo hover:transform hover:scale-150"
-            href="https://github.com/kevsaj"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Kevin Sajan
-          </a>
-        </div>
-      </header>
+    <div className="flex flex-col">
+      <Header />
     </div>
   );
 }
